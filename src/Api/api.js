e70bdbc12960a3c6b152c1f7970c37a9add8710c
moyoussef11 <mohmedyoussef472@gmail.com>
2025-07-15 15:44:35 +0300

    first commit

---
+++ src/Api/api.js
@@ -0,0 +1,21 @@
+const BASEURL = "https://doctor-appoinment-app-green.vercel.app/api";
+const AUTH = "auth";
+const REGISTER = "register";
+const LOGIN = "login";
+const USERS = "users";
+const DOCTORS = "doctors";
+const BOOK_APPOINTMENT = "book-appointment";
+const MY_APPOINTMENTS = "my-appointments";
+const APPOINTMENTS = "appointments";
+
+export {
+  BASEURL,
+  AUTH,
+  REGISTER,
+  LOGIN,
+  USERS,
+  DOCTORS,
+  BOOK_APPOINTMENT,
+  MY_APPOINTMENTS,
+  APPOINTMENTS,
+};
